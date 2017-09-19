1e05c85c308d104100184c23fd37b88462a41dfa
iscream1 <iscream.94@gmail.com>
2017-09-20 01:22:49 +0200

    atrendezo working

--- main.cpp
+++ main.cpp
@@ -15,24 +15,50 @@ int convert(const int S, const int A)
         return (S % A + 10 * convert(S / A, A));
     }
 }
+//from slide
+list append(const list L1, const list L2)
+{
+    if (L1 == nil) return L2;
+    return cons(hd(L1), append(tl(L1), L2));
+}
 
+//from slide
+list nrev(const list L) {
+    if (L == nil) return nil;
+    return append(nrev(tl(L)), cons(hd(L), nil));
+}
+
+//returns a list with the head 0
 list convertx(const list l, const int base)
 {
-    write(l.head());
-    write(l);cout<<endl;
     if (l.head() == 0)
     {
         return l;
     }
-    else
-    {
-        return convertx(
+    return convertx(
                              cons(l.head()/base,
                              cons(
                                   l.head()%base, l.tail()
                                   ))
                              , base);
-    }
+}
+
+int length(const list l)
+{
+    if(l==nil) return 0;
+    return 1+length(l.tail());
+}
+
+list atrendezox(const list l, const list plan, const list ps)
+{
+    if(length(l)==0) return append(plan, ps);
+    if(length(l)%2==1) return atrendezox(l.tail(), cons(l.head(), plan), ps);
+    return atrendezox(l.tail(), plan, cons(l.head(), ps));
+}
+
+list atrendezo(const list l)
+{
+    return atrendezox(nrev(l), nil, nil);
 }
 
 /*int atrendezett(const int S, const int A)
@@ -43,7 +69,9 @@ list convertx(const list l, const int base)
 int main()
 {
     //cout<<atrendezett(100, 2);
-    write(convertx(cons(100, nil), 2));
+    write(atrendezo(
+                    convertx(cons(100, nil), 2).tail())
+          );
     //convertx(cons(100, nil), 2);
     return 0;
 }
